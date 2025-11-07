47e91d64402050f44d88b31353ea229090bd610a
Jiliar Silgado <Jiliar.Silgado.-ND@disney.com>
2025-11-07 11:42:59 -0500

    fixing

--- projects/back-ms-movies-webflux/init-db.sql
+++
@@ -1,3 +0,0 @@
--- Create database if it doesn't exist
-SELECT 'CREATE DATABASE back_ms_movies_webflux_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'back_ms_movies_webflux_db')\gexec

74e5f4d916ab85f744d0088ce468644816418464
Jiliar Silgado <Jiliar.Silgado.-ND@disney.com>
2025-11-07 07:17:12 -0500

    fixings

--- projects/back-ms-movies/init-db.sql
+++
@@ -1,3 +0,0 @@
--- Create database if it doesn't exist
-SELECT 'CREATE DATABASE back_ms_movies_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'back_ms_movies_db')\gexec
--- projects/back-ms-users/init-db.sql
+++
@@ -1,2 +0,0 @@
--- Create database
-CREATE DATABASE back_ms_users_db;
